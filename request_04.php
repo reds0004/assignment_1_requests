35027bbfa2e3e7ae6a0547dafb788b634ba29da7
Allen Redshaw <reds0004@algonquinlive.com>
2023-10-01 20:41:46 -0400

    Complete 4 and 5

--- request_04.php
+++ request_04.php
@@ -1,1 +1,16 @@
 By using the date() function, display 5 dates: 100 years ago, 100 days ago, today, 100 days from today, 100 months from today. Try to use as many variables as possible to avoid repetition of time calculations.
+<?php 
+// making variables, with the requested time frames (strtotime used to convert dates into UNIX TIMESTAMPS)
+$oneHundredYearsAgo = date("Y-m-d", strtotime("-100 years"));
+$oneHundredDaysAgo = date("Y-m-d", strtotime("-100 days"));
+$oneHundredDaysFromNow = date("Y-m-d", strtotime("+100 days"));
+$oneHundredMonthsFromNow = date("Y-m-d", strtotime("+100 months"));
+$todaysDate = date("Y-m-d");
+
+// Displays the requested dates, on a new line becasue the the <br>
+echo "100 Years ago: " . $oneHundredYearsAgo . "<br>";
+echo "100 Days ago: " . $oneHundredDaysAgo . "<br>";
+echo "100 days from now: " . $oneHundredDaysFromNow . "<br>";
+echo "100 months from now: " . $oneHundredMonthsFromNow . "<br>";
+echo "Today's date is: " . $todaysDate . "<br>";
+?>
--- request_05.php
+++ request_05.php
@@ -1,1 +1,28 @@
 Create a page in HTML and PHP. The page must display a bulleted list of 10 names. The names must come from an array. You have to use foreach.
+<html>
+    <head>Request_5</head>
+    <body>
+        <h1>PHP Request 5</h1>
+        <ul>
+            <!-- defining 10 names in the array -->
+            <?php 
+            $tenNames = array(
+                "Peter",
+                "Louise",
+                "Stewey",
+                "Chris",
+                "Meg",
+                "Brian",
+                "Joe",
+                "Quagmire",
+                "Cleveland",
+                "Rick James"
+            );
+            // using the foreach loop to display each name as a list item
+            foreach($tenNames as $tenNames) {
+                echo "<li>$tenNames</li>";
+            }
+            ?>
+        </ul>
+    </body>
+</html>
